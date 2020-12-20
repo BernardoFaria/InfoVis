cb173d6fcb97ec08d022c6bb0ded1064f1034fc9
BernardoFaria <bernardo.faria@tecnico.ulisboa.pt>
2020-12-20 01:11:19 +0000

    Fixing lollipop values

--- js/lollipop.js
+++ js/lollipop.js
@@ -23,7 +23,7 @@ var yAxis;
 // datasets variables
 var artists;
 
-var auxDec = [1910, 1920, 1930, 1940, 1950, 1960, 1970, 1980, 1990, 2000, 2010, 2020];  // aux with all decades
+var auxDec = [1900, 1910, 1920, 1930, 1940, 1950, 1960, 1970, 1980, 1990, 2000, 2010];  // aux with all decades
 var id_line = [0,1,2,3,4,5,6,7,8,9,10,11];  // id's for lollipop's lines
 var id_circle = [12,13,14,15,16,17,18,19,20,21,22,23]; // id's for lollipop's circles ; + 12
 
@@ -39,18 +39,18 @@ dispatchClickBar_Lollipop.on("clickBar", function(artistSelected) {
 
    var flagVec = [0,0,0,0,0,0,0,0,0,0,0,0];    // couldn't find a better way again
     
-   if (artistSelected.creationDate > 1900 && artistSelected.creationDate <= 1910) { flagVec[0]++; }
-   if (artistSelected.creationDate > 1910 && artistSelected.creationDate <= 1920) { flagVec[1]++; }
-   if (artistSelected.creationDate > 1920 && artistSelected.creationDate <= 1930) { flagVec[2]++; }
-   if (artistSelected.creationDate > 1930 && artistSelected.creationDate <= 1940) { flagVec[3]++; }
-   if (artistSelected.creationDate > 1940 && artistSelected.creationDate <= 1950) { flagVec[4]++; }
-   if (artistSelected.creationDate > 1950 && artistSelected.creationDate <= 1960) { flagVec[5]++; }
-   if (artistSelected.creationDate > 1960 && artistSelected.creationDate <= 1970) { flagVec[6]++; }
-   if (artistSelected.creationDate > 1970 && artistSelected.creationDate <= 1980) { flagVec[7]++; }
-   if (artistSelected.creationDate > 1980 && artistSelected.creationDate <= 1990) { flagVec[8]++; }
-   if (artistSelected.creationDate > 1990 && artistSelected.creationDate <= 2000) { flagVec[9]++; }
-   if (artistSelected.creationDate > 2000 && artistSelected.creationDate <= 2010) { flagVec[10]++; }
-   if (artistSelected.creationDate > 2010 && artistSelected.creationDate <= 2020) { flagVec[11]++; }
+   if (artistSelected.creationDate >= 1900 && artistSelected.creationDate < 1910) { flagVec[0]++; }
+   if (artistSelected.creationDate >= 1910 && artistSelected.creationDate < 1920) { flagVec[1]++; }
+   if (artistSelected.creationDate >= 1920 && artistSelected.creationDate < 1930) { flagVec[2]++; }
+   if (artistSelected.creationDate >= 1930 && artistSelected.creationDate < 1940) { flagVec[3]++; }
+   if (artistSelected.creationDate >= 1940 && artistSelected.creationDate < 1950) { flagVec[4]++; }
+   if (artistSelected.creationDate >= 1950 && artistSelected.creationDate < 1960) { flagVec[5]++; }
+   if (artistSelected.creationDate >= 1960 && artistSelected.creationDate < 1970) { flagVec[6]++; }
+   if (artistSelected.creationDate >= 1970 && artistSelected.creationDate < 1980) { flagVec[7]++; }
+   if (artistSelected.creationDate >= 1980 && artistSelected.creationDate < 1990) { flagVec[8]++; }
+   if (artistSelected.creationDate >= 1990 && artistSelected.creationDate < 2000) { flagVec[9]++; }
+   if (artistSelected.creationDate >= 2000 && artistSelected.creationDate < 2010) { flagVec[10]++; }
+   if (artistSelected.creationDate >= 2010 && artistSelected.creationDate < 2020) { flagVec[11]++; }
 
    var idAux = flagVec.indexOf(1)
 
@@ -71,7 +71,7 @@ dispatchClickBar_Lollipop.on("clickBar", function(artistSelected) {
          toolTip.transition()
                .duration(500)
                .style("opacity", 0.9);
-         var text = artistSelected.displayName + " Birth Date: " + artistSelected.creationDate;
+         var text = artistSelected.displayName + " Creation Date: " + artistSelected.creationDate;
          toolTip.html(text)
                .style("left", (x) + "px")
                .style("top", (y + 470) + "px");
@@ -84,7 +84,7 @@ dispatchClickBar_Lollipop.on("clickBar", function(artistSelected) {
          toolTip.transition()
                .duration(500)
                .style("opacity", 0.9);
-         var text = artistSelected.displayName + " Birth Date: " + artistSelected.creationDate;
+         var text = artistSelected.displayName + " Creation Date: " + artistSelected.creationDate;
          toolTip.html(text)
                .style("left", (x) + "px")
                .style("top", (y + 470) + "px");
@@ -115,18 +115,18 @@ dispatchClickLine_Lollipop.on("clickLine", function(genreSelected) {
    // get total artists per decade
    var getTotalArtists = [0,0,0,0,0,0,0,0,0,0,0,0];    // couldn't find a better way
    for(var i = 0; i < Object.keys(filteredDataUpdate).length; i++) {    
-      if (filteredDataUpdate[i].creationDate > 1900 && filteredDataUpdate[i].creationDate <= 1910) { getTotalArtists[0]++; }
-      if (filteredDataUpdate[i].creationDate > 1910 && filteredDataUpdate[i].creationDate <= 1920) { getTotalArtists[1]++; }
-      if (filteredDataUpdate[i].creationDate > 1920 && filteredDataUpdate[i].creationDate <= 1930) { getTotalArtists[2]++; }
-      if (filteredDataUpdate[i].creationDate > 1930 && filteredDataUpdate[i].creationDate <= 1940) { getTotalArtists[3]++; }
-      if (filteredDataUpdate[i].creationDate > 1940 && filteredDataUpdate[i].creationDate <= 1950) { getTotalArtists[4]++; }
-      if (filteredDataUpdate[i].creationDate > 1950 && filteredDataUpdate[i].creationDate <= 1960) { getTotalArtists[5]++; }
-      if (filteredDataUpdate[i].creationDate > 1960 && filteredDataUpdate[i].creationDate <= 1970) { getTotalArtists[6]++; }
-      if (filteredDataUpdate[i].creationDate > 1970 && filteredDataUpdate[i].creationDate <= 1980) { getTotalArtists[7]++; }
-      if (filteredDataUpdate[i].creationDate > 1980 && filteredDataUpdate[i].creationDate <= 1990) { getTotalArtists[8]++; }
-      if (filteredDataUpdate[i].creationDate > 1990 && filteredDataUpdate[i].creationDate <= 2000) { getTotalArtists[9]++; }
-      if (filteredDataUpdate[i].creationDate > 2000 && filteredDataUpdate[i].creationDate <= 2010) { getTotalArtists[10]++; }
-      if (filteredDataUpdate[i].creationDate > 2010 && filteredDataUpdate[i].creationDate <= 2020) { getTotalArtists[11]++; }
+      if (filteredDataUpdate[i].creationDate >= 1900 && filteredDataUpdate[i].creationDate < 1910) { getTotalArtists[0]++; }
+      if (filteredDataUpdate[i].creationDate >= 1910 && filteredDataUpdate[i].creationDate < 1920) { getTotalArtists[1]++; }
+      if (filteredDataUpdate[i].creationDate >= 1920 && filteredDataUpdate[i].creationDate < 1930) { getTotalArtists[2]++; }
+      if (filteredDataUpdate[i].creationDate >= 1930 && filteredDataUpdate[i].creationDate < 1940) { getTotalArtists[3]++; }
+      if (filteredDataUpdate[i].creationDate >= 1940 && filteredDataUpdate[i].creationDate < 1950) { getTotalArtists[4]++; }
+      if (filteredDataUpdate[i].creationDate >= 1950 && filteredDataUpdate[i].creationDate < 1960) { getTotalArtists[5]++; }
+      if (filteredDataUpdate[i].creationDate >= 1960 && filteredDataUpdate[i].creationDate < 1970) { getTotalArtists[6]++; }
+      if (filteredDataUpdate[i].creationDate >= 1970 && filteredDataUpdate[i].creationDate < 1980) { getTotalArtists[7]++; }
+      if (filteredDataUpdate[i].creationDate >= 1980 && filteredDataUpdate[i].creationDate < 1990) { getTotalArtists[8]++; }
+      if (filteredDataUpdate[i].creationDate >= 1990 && filteredDataUpdate[i].creationDate < 2000) { getTotalArtists[9]++; }
+      if (filteredDataUpdate[i].creationDate >= 2000 && filteredDataUpdate[i].creationDate < 2010) { getTotalArtists[10]++; }
+      if (filteredDataUpdate[i].creationDate >= 2010 && filteredDataUpdate[i].creationDate < 2020) { getTotalArtists[11]++; }
    }
    // join [decade, totalArtists]
    fullDataset = getTotalArtists.map(function(d, i) {
@@ -221,18 +221,18 @@ dispatchClickMap_Lollipop.on("clickMap", function(countrySelected) {
    // get total artists per decade
    var getTotalArtists = [0,0,0,0,0,0,0,0,0,0,0,0];    // couldn't find a better way
    for(var i = 0; i < Object.keys(filteredDataUpdate).length; i++) {  
-      if (filteredDataUpdate[i].creationDate > 1900 && filteredDataUpdate[i].creationDate <= 1910) { getTotalArtists[0]++; }
-      if (filteredDataUpdate[i].creationDate > 1910 && filteredDataUpdate[i].creationDate <= 1920) { getTotalArtists[1]++; }
-      if (filteredDataUpdate[i].creationDate > 1920 && filteredDataUpdate[i].creationDate <= 1930) { getTotalArtists[2]++; }
-      if (filteredDataUpdate[i].creationDate > 1930 && filteredDataUpdate[i].creationDate <= 1940) { getTotalArtists[3]++; }
-      if (filteredDataUpdate[i].creationDate > 1940 && filteredDataUpdate[i].creationDate <= 1950) { getTotalArtists[4]++; }
-      if (filteredDataUpdate[i].creationDate > 1950 && filteredDataUpdate[i].creationDate <= 1960) { getTotalArtists[5]++; }
-      if (filteredDataUpdate[i].creationDate > 1960 && filteredDataUpdate[i].creationDate <= 1970) { getTotalArtists[6]++; }
-      if (filteredDataUpdate[i].creationDate > 1970 && filteredDataUpdate[i].creationDate <= 1980) { getTotalArtists[7]++; }
-      if (filteredDataUpdate[i].creationDate > 1980 && filteredDataUpdate[i].creationDate <= 1990) { getTotalArtists[8]++; }
-      if (filteredDataUpdate[i].creationDate > 1990 && filteredDataUpdate[i].creationDate <= 2000) { getTotalArtists[9]++; }
-      if (filteredDataUpdate[i].creationDate > 2000 && filteredDataUpdate[i].creationDate <= 2010) { getTotalArtists[10]++; }
-      if (filteredDataUpdate[i].creationDate > 2010 && filteredDataUpdate[i].creationDate <= 2020) { getTotalArtists[11]++; }
+      if (filteredDataUpdate[i].creationDate >= 1900 && filteredDataUpdate[i].creationDate < 1910) { getTotalArtists[0]++; }
+      if (filteredDataUpdate[i].creationDate >= 1910 && filteredDataUpdate[i].creationDate < 1920) { getTotalArtists[1]++; }
+      if (filteredDataUpdate[i].creationDate >= 1920 && filteredDataUpdate[i].creationDate < 1930) { getTotalArtists[2]++; }
+      if (filteredDataUpdate[i].creationDate >= 1930 && filteredDataUpdate[i].creationDate < 1940) { getTotalArtists[3]++; }
+      if (filteredDataUpdate[i].creationDate >= 1940 && filteredDataUpdate[i].creationDate < 1950) { getTotalArtists[4]++; }
+      if (filteredDataUpdate[i].creationDate >= 1950 && filteredDataUpdate[i].creationDate < 1960) { getTotalArtists[5]++; }
+      if (filteredDataUpdate[i].creationDate >= 1960 && filteredDataUpdate[i].creationDate < 1970) { getTotalArtists[6]++; }
+      if (filteredDataUpdate[i].creationDate >= 1970 && filteredDataUpdate[i].creationDate < 1980) { getTotalArtists[7]++; }
+      if (filteredDataUpdate[i].creationDate >= 1980 && filteredDataUpdate[i].creationDate < 1990) { getTotalArtists[8]++; }
+      if (filteredDataUpdate[i].creationDate >= 1990 && filteredDataUpdate[i].creationDate < 2000) { getTotalArtists[9]++; }
+      if (filteredDataUpdate[i].creationDate >= 2000 && filteredDataUpdate[i].creationDate < 2010) { getTotalArtists[10]++; }
+      if (filteredDataUpdate[i].creationDate >= 2010 && filteredDataUpdate[i].creationDate < 2020) { getTotalArtists[11]++; }
    }
    // join [decade, totalArtists]
    fullDataset = getTotalArtists.map(function(d, i) {
@@ -323,18 +323,18 @@ function gen_lollipop() {
     // get total artists per decade
     var getTotalArtists = [0,0,0,0,0,0,0,0,0,0,0,0];    // couldn't find a better way
     for(var i = 0; i < Object.keys(artists).length-1; i++) {    
-        if (artists[i].creationDate > 1900 && artists[i].creationDate <= 1910) { getTotalArtists[0]++; }
-        if (artists[i].creationDate > 1910 && artists[i].creationDate <= 1920) { getTotalArtists[1]++; }
-        if (artists[i].creationDate > 1920 && artists[i].creationDate <= 1930) { getTotalArtists[2]++; }
-        if (artists[i].creationDate > 1930 && artists[i].creationDate <= 1940) { getTotalArtists[3]++; }
-        if (artists[i].creationDate > 1940 && artists[i].creationDate <= 1950) { getTotalArtists[4]++; }
-        if (artists[i].creationDate > 1950 && artists[i].creationDate <= 1960) { getTotalArtists[5]++; }
-        if (artists[i].creationDate > 1960 && artists[i].creationDate <= 1970) { getTotalArtists[6]++; }
-        if (artists[i].creationDate > 1970 && artists[i].creationDate <= 1980) { getTotalArtists[7]++; }
-        if (artists[i].creationDate > 1980 && artists[i].creationDate <= 1990) { getTotalArtists[8]++; }
-        if (artists[i].creationDate > 1990 && artists[i].creationDate <= 2000) { getTotalArtists[9]++; }
-        if (artists[i].creationDate > 2000 && artists[i].creationDate <= 2010) { getTotalArtists[10]++; }
-        if (artists[i].creationDate > 2010 && artists[i].creationDate <= 2020) { getTotalArtists[11]++; }
+        if (artists[i].creationDate >= 1900 && artists[i].creationDate < 1910) { getTotalArtists[0]++; }
+        if (artists[i].creationDate >= 1910 && artists[i].creationDate < 1920) { getTotalArtists[1]++; }
+        if (artists[i].creationDate >= 1920 && artists[i].creationDate < 1930) { getTotalArtists[2]++; }
+        if (artists[i].creationDate >= 1930 && artists[i].creationDate < 1940) { getTotalArtists[3]++; }
+        if (artists[i].creationDate >= 1940 && artists[i].creationDate < 1950) { getTotalArtists[4]++; }
+        if (artists[i].creationDate >= 1950 && artists[i].creationDate < 1960) { getTotalArtists[5]++; }
+        if (artists[i].creationDate >= 1960 && artists[i].creationDate < 1970) { getTotalArtists[6]++; }
+        if (artists[i].creationDate >= 1970 && artists[i].creationDate < 1980) { getTotalArtists[7]++; }
+        if (artists[i].creationDate >= 1980 && artists[i].creationDate < 1990) { getTotalArtists[8]++; }
+        if (artists[i].creationDate >= 1990 && artists[i].creationDate < 2000) { getTotalArtists[9]++; }
+        if (artists[i].creationDate >= 2000 && artists[i].creationDate < 2010) { getTotalArtists[10]++; }
+        if (artists[i].creationDate >= 2010 && artists[i].creationDate < 2020) { getTotalArtists[11]++; }
     }
     // join [decade, totalArtists]
     fullDataset = getTotalArtists.map(function(d, i) {
@@ -384,6 +384,7 @@ function gen_lollipop() {
        .enter()
        .append("line")
        .attr("class", "lines-lollipop")
+      //  .attr("transform", "translate(" + (-26) + ",0)")
        .attr("x1", function(d) { return xScale(d.decade); })
        .attr("x2", function(d) { return xScale(d.decade); })
        .attr("y1", yScale(0))
@@ -407,6 +408,7 @@ function gen_lollipop() {
        .enter()
        .append("circle")
        .attr("class", "circle-lollipop")
+      //  .attr("transform", "translate(" + (-26) + ",0)")
        .attr("cx", function(d) { return xScale(d.decade); })
        .attr("cy", yScale(0))
        .attr("r", radius)
